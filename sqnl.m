f8f8903eb41cdf1dd89a3186bb2a57b3c5ac88ac
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2020-04-13 13:24:41 +1200

    Create sqnl.m

---
+++ sqnl.m
@@ -0,0 +1,18 @@
+function a = sqnl(n)
+
+
+a=zeros(size(n));
+
+idx = find(n > 2);
+n(idx) = 2;
+
+idx = find(n < -2);
+n(idx) = -2;
+
+
+idx = find(n >= 0);
+a(idx) = n(idx) - n(idx).^2/4;
+
+idx = find(n < 0);
+a(idx) = n(idx) + n(idx).^2/4;
+end
