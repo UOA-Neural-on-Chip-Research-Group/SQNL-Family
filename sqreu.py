d5fc1bd255e2d74d4570fe0443ea8303c14cefcf
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2020-07-10 13:51:06 +1200

    Update sqreu.py

--- sqreu.py
+++ sqreu.py
@@ -1,3 +1,9 @@
 import tensorflow as tf
 import numpy as np
 import keras
+
+def sqreu(x):    
+    u=tf.clip_by_value(x,-2,200)    
+    a = u    alpha = 1.0    
+    t = alpha*(a + 2.0*(a*a)/4.0)      
+    return tf.maximum(0.0, a) + tf.minimum(0.0,t)
